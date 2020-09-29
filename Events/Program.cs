a812ddb5e6f2cd3c89345e71d22ccf902de70ea2
Ewerton Mattos <ewerton.sc@gmail.com>
2020-09-29 11:22:13 -0300

    git test

--- Events/Program.cs
+++ Events/Program.cs
@@ -11,7 +11,7 @@ namespace Events
      An event is a notification sent by an object to signal the occurrence of an action. Events in .NET follow the observer design pattern.
     The class who raises events is called Publisher, and the class who receives the notification is called Subscriber. There can be multiple subscribers of a single event. Typically, a publisher raises an event when some action occurred. The subscribers, who are interested in getting a notification when an action occurred, should register with an event and handle it.
     In C#, an event is an encapsulated delegate. It is dependent on the delegate. The delegate defines the signature for the event handler method of the subscriber class.
-
+      
      */
     public class Program
     {
